7025f019586e5e5bbe4fe1590e3fa1564dabdc1b
Jutololla <julianangulop@gmail.com>
2025-04-21 11:40:09 -0500

    feat: implement NewUserForm component with local storage and validation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,12 @@
-import React, { ReactElement, ReactNode } from 'react';
-import { ConfigProvider } from 'antd';
-import type { AppProps } from 'next/app';
-import theme from '@/theme/themeConfig';
-import { NextPage } from 'next';
-import '@ant-design/v5-patch-for-react-19';
+import React, { ReactElement, ReactNode } from "react";
+import { ConfigProvider } from "antd";
+import type { AppProps } from "next/app";
+import theme from "@/theme/themeConfig";
+import { NextPage } from "next";
+import "@ant-design/v5-patch-for-react-19";
+import es_ES from "antd/locale/es_ES";
 
-export type NextPageWithLayout<P = {}, IP = P> = NextPage<
-  P,
-  IP
-> & {
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
 };
 type AppPropsWithLayout = AppProps & {
@@ -18,10 +16,10 @@ const App = ({ Component, pageProps }: AppPropsWithLayout) => {
   const getLayout = Component.getLayout ?? ((page) => page);
 
   return (
-    <ConfigProvider theme={theme}>
+    <ConfigProvider theme={theme} locale={es_ES}>
       {getLayout(<Component {...pageProps} />)}
     </ConfigProvider>
-  )
+  );
 };
 
 export default App;
