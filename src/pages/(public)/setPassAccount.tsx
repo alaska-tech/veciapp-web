5ace500e94f2f9ce527fe5672b50de87ade57071
esarmiem <eldersarmiento1@gmail.com>
2025-04-26 11:11:25 -0500

    ADD setpass & validateaccount screens

---
+++ src/pages/(public)/setPassAccount.tsx
@@ -0,0 +1,45 @@
+import LandingPageLayout from "@/components/layout/LandingPageLayout";
+import { LockOutlined } from "@ant-design/icons";
+import { Form, Input, Button } from "antd";
+import React, { ReactElement } from "react";
+interface FormValues {
+  email: string;
+  code: string;
+}
+const Index = () => {
+  const [formInstance] = Form.useForm();
+  const handleSubmit = async () => {
+    try {
+      const values = formInstance.getFieldsValue();
+      //await formInstance.validateFields();
+      //await verifyCode.mutateAsync(values);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  return (
+    <div>
+      <Form<FormValues>
+        form={formInstance}
+        onFinish={handleSubmit}
+        style={{ minWidth: 300 }}
+      >
+        <Form.Item label="Email" name="email" rules={[{ required: true }]}>
+        <Input.Password placeholder="Contraseña" />
+        </Form.Item>
+        <Form.Item label="Code" name="code" rules={[{ required: true }]}>
+        <Input.Password placeholder="Confirma tu Contraseña" />
+        </Form.Item>
+        <Button htmlType="submit" type="primary" icon={<LockOutlined />}>
+          Confirm
+        </Button>
+      </Form>
+    </div>
+  );
+};
+
+export default Index;
+
+Index.getLayout = function getLayout(page: ReactElement) {
+  return <LandingPageLayout backButton={true}> {page}</LandingPageLayout>;
+};
---
+++ src/pages/(public)/validateAccount.tsx
@@ -0,0 +1,45 @@
+import LandingPageLayout from "@/components/layout/LandingPageLayout";
+import { LockOutlined } from "@ant-design/icons";
+import { Form, Input, Button } from "antd";
+import React, { ReactElement } from "react";
+import { useRouter } from "next/router";
+interface FormValues {
+  email: string;
+  code: string;
+}
+const Index = () => {
+    const router = useRouter();
+  const [formInstance] = Form.useForm();
+  const handleSubmit = async () => {
+    router.push("/(public)/setPassAccount");
+    try {
+      const values = formInstance.getFieldsValue();
+      //await formInstance.validateFields();
+      //await verifyCode.mutateAsync(values);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  return (
+    <div>
+      <Form<FormValues>
+        form={formInstance}
+        onFinish={handleSubmit}
+        style={{ minWidth: 300 }}
+      >
+        <Form.Item label="Email" name="email" rules={[{ required: true }]}>
+        <Input.OTP />
+        </Form.Item>
+        <Button htmlType="submit" type="primary" icon={<LockOutlined />}>
+          Confirm
+        </Button>
+      </Form>
+    </div>
+  );
+};
+
+export default Index;
+
+Index.getLayout = function getLayout(page: ReactElement) {
+  return <LandingPageLayout backButton={true}> {page}</LandingPageLayout>;
+};
