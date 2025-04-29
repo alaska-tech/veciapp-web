2e0a5c6ea15d9518c8494235a1fd8f7f07ce3ddb
Jutololla <julianangulop@gmail.com>
2025-04-28 19:35:16 -0500

    UPDATE: Refactor code structure for improved readability and maintainability

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  transpilePackages: [
+    'rc-util',
+    "rc-picker",
+    "rc-pagination",
+    "@ant-design/icons-svg"
+  ]
 };
 
 export default nextConfig;
