4f2b609b8e8112f2306ad1a5e44834f3ebdb7cc2
Jutololla <julianangulop@gmail.com>
2025-06-27 07:35:02 -0500

    UPDATE: Enhance new product service form by adding image upload functionality; implement preview feature for uploaded images; refactor form structure to improve usability and maintainability.

--- src/components/forms/newProductServiceForm.tsx
+++ src/components/forms/newProductServiceForm.tsx
@@ -1,24 +1,69 @@
-import { Form, Input, InputNumber, Radio, Select } from "antd";
-import React from "react";
+import {
+  Form,
+  GetProp,
+  Input,
+  InputNumber,
+  Radio,
+  Select,
+  Upload,
+  UploadFile,
+  UploadProps,
+  Image,
+} from "antd";
+import React, { useState } from "react";
 import { ProductService } from "@models";
 import FormWrapper from "./formWrapper";
 import { getUserInfo } from "@/actions/localStorage.actions";
 import { useBranchAction } from "@/actions/branch.action";
+import { PlusOutlined } from "@ant-design/icons";
 
 type productServiceWithAuxProps = ProductService;
 
+type FileType = Parameters<GetProp<UploadProps, "beforeUpload">>[0];
+
+const getBase64 = (file: FileType): Promise<string> =>
+  new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result as string);
+    reader.onerror = (error) => reject(error);
+  });
 function parseInitialValues(values: ProductService) {
   return { ...values };
 }
+const MAX_NUMBER_OF_PHOTOS = 8 as const;
 export const FormElement = <T extends productServiceWithAuxProps>(props: {
   onFinish?: (values: T) => Promise<void>;
   loading?: boolean;
   initialValues?: T;
+  branchId: string;
 }) => {
   const hasInitialValues: boolean = !!props.initialValues;
   const user = getUserInfo();
-  const branchActions = useBranchAction();
-  const branchesQuery = branchActions.getBranchs();
+  //----
+  const [previewOpen, setPreviewOpen] = useState(false);
+  const [previewImage, setPreviewImage] = useState("");
+  const [fileList, setFileList] = useState<UploadFile[]>([]);
+
+  const handlePreview = async (file: UploadFile) => {
+    if (!file.url && !file.preview) {
+      file.preview = await getBase64(file.originFileObj as FileType);
+    }
+
+    setPreviewImage(file.url || (file.preview as string));
+    setPreviewOpen(true);
+  };
+
+  const handleChange: UploadProps["onChange"] = ({ fileList: newFileList }) =>
+    setFileList(newFileList);
+
+  const uploadButton = (
+    <button style={{ border: 0, background: "none" }} type="button">
+      <PlusOutlined />
+      <div style={{ marginTop: 8 }}>Cargar foto</div>
+    </button>
+  );
+  //----
   const handleFinish = async (values: T) => {
     const { ...rest } = values;
     const mappedValues = {
@@ -66,22 +111,6 @@ export const FormElement = <T extends productServiceWithAuxProps>(props: {
           >
             <Input disabled={hasInitialValues} />
           </Form.Item>
-          <Form.Item
-            name="branchId"
-            label="Tienda"
-            rules={[
-              {
-                required: true,
-              },
-            ]}
-          >
-            <Select
-              options={branchesQuery.data?.data.data.map((branch) => {
-                return { label: branch.name, value: branch.id };
-              })}
-            />
-          </Form.Item>
-
           <Form.Item
             name="categoryId"
             label="Categoria"
@@ -166,6 +195,29 @@ export const FormElement = <T extends productServiceWithAuxProps>(props: {
             ]}
           >
             <InputNumber min={0} />
+          </Form.Item>
+          <Form.Item>
+            <Upload
+              action={`https://veciapp-backend.onrender.com/api/v1/productservice/upload-images/${props.branchId}`}
+              listType="picture-card"
+              fileList={fileList}
+              onPreview={handlePreview}
+              onChange={handleChange}
+            >
+              {fileList.length >= MAX_NUMBER_OF_PHOTOS ? null : uploadButton}
+            </Upload>
+            {previewImage && (
+              <Image
+                wrapperStyle={{ display: "none" }}
+                preview={{
+                  visible: previewOpen,
+                  onVisibleChange: (visible) => setPreviewOpen(visible),
+                  afterOpenChange: (visible) => !visible && setPreviewImage(""),
+                }}
+                src={previewImage}
+                alt=""
+              />
+            )}
           </Form.Item>
         </div>
       )}
