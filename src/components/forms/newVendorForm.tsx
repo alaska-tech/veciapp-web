0568e182b8ebc8f51b8d8dd65b2000029ee408a2
esarmiem <eldersarmiento1@gmail.com>
2025-07-19 19:00:40 -0500

    FIX new vendor form ui in desktop

--- src/components/forms/newVendorForm.tsx
+++ src/components/forms/newVendorForm.tsx
@@ -1,4 +1,4 @@
-import { Divider, Form, Input, InputNumber, Radio } from "antd";
+import { Divider, Form, Input, InputNumber, Radio, Row, Col } from "antd";
 import React from "react";
 import { Vendor, VendorGenders } from "@models";
 import FormWrapper from "./formWrapper";
@@ -70,269 +70,298 @@ export const FormElement = <T extends vendorWithAuxProps>(props: {
       preserveDataInCache={!hasInitialValues}
     >
       {(formInstance) => (
-        <div
-          style={{
-            display: "flex",
-            flexWrap: "wrap",
-            gap: "60px",
-          }}
-        >
-          <div style={{ flex: `1 1 ${columnMinWidth}` }}>
+        <Row gutter={[32, 24]}>
+          {/* Columna izquierda - Información personal */}
+          <Col xs={24} lg={12}>
             <Divider>Información personal</Divider>
-            <Form.Item
-              name="internalCode"
-              label="Código interno"
-              tooltip="Escriba el código interno de la fundación Maleua"
-              rules={[
-                {
-                  required: true,
-                },
-                {
-                  max: 100,
-                },
-              ]}
-            >
-              <Input disabled={hasInitialValues} />
-            </Form.Item>
-            <Form.Item
-              name="fullName"
-              label="Nombre completo"
-              rules={[
-                {
-                  required: true,
-                },
-                {
-                  max: 255,
-                },
-              ]}
-            >
-              <Input disabled={hasInitialValues} />
-            </Form.Item>
-            <Form.Item
-              name="identification"
-              label="Número de identidad"
-              tooltip="Escriba el número de identidad sin puntos ni espacios"
-              rules={[
-                {
-                  pattern: /^[0-9]+$/,
-                  message: "El número de identidad solo puede contener números",
-                },
-                {
-                  required: true,
-                },
-                {
-                  max: 100,
-                },
-              ]}
-            >
-              <Input disabled={hasInitialValues} />
-            </Form.Item>
-            <Form.Item
-              name="email"
-              label="E-mail"
-              rules={[
-                {
-                  type: "email",
-                },
-                {
-                  required: true,
-                },
-                {
-                  max: 150,
-                },
-              ]}
-            >
-              <Input disabled={hasInitialValues} />
-            </Form.Item>
-            <Form.Item
-              name="cellphone"
-              label="Teléfono celular"
-              tooltip="Escriba el número de celular sin puntos ni espacios"
-              rules={[
-                { required: true },
-                {
-                  pattern: /^[0-9]+$/,
-                  message: "El teléfono solo puede contener números",
-                },
-                {
-                  max: 20,
-                },
-              ]}
-            >
-              <Input
-                addonBefore={
-                  <Form.Item name="prefix" rules={[{ required: true }]} noStyle>
-                    <CustomSelectWithInput
-                      selectProps={{
-                        options: [
-                          {
-                            value: "57",
-                            label: "+57",
-                          },
-                        ],
-                        style: { width: 75 },
-                        popupMatchSelectWidth: false,
-                      }}
-                      inputProps={{
-                        placeholder: "Escriba...",
-                        style: {
-                          width: 65,
-                        },
-                      }}
-                    />
-                  </Form.Item>
-                }
-                style={{ width: "100%" }}
-              />
-            </Form.Item>
-            <Form.Item
-              name="address"
-              label="Dirección"
-              rules={[
-                {
-                  required: true,
-                },
-                {
-                  max: 255,
-                },
-              ]}
-            >
-              <Input />
-            </Form.Item>
-            <Form.Item
-              name="age"
-              label="Edad"
-              rules={[
-                {
-                  type: "number",
-                  min: 0,
-                  max: 120,
-                },
-                {
-                  type: "number",
-                },
-              ]}
-            >
-              <InputNumber min={0} />
-            </Form.Item>
-            <Form.Item
-              name="gender"
-              label="Género"
-              rules={[{ type: "enum", enum: Object.values(VendorGenders) }]}
-            >
-              <Radio.Group
-                options={[
-                  { value: "M", label: "Hombre" },
-                  { value: "F", label: "Mujer" },
-                  { value: "O", label: "Otro" },
-                ]}
-              />
-            </Form.Item>
-            <Form.Item name={"bio"} label="Biografía" rules={[]}>
-              <Input.TextArea />
-            </Form.Item>
-          </div>
-          <div style={{ flex: `1 1 ${columnMinWidth}` }}>
+            <Row gutter={[16, 16]}>
+              <Col span={24}>
+                <Form.Item
+                  name="internalCode"
+                  label="Código interno"
+                  tooltip="Escriba el código interno de la fundación Maleua"
+                  rules={[
+                    {
+                      required: true,
+                    },
+                    {
+                      max: 100,
+                    },
+                  ]}
+                >
+                  <Input disabled={hasInitialValues} />
+                </Form.Item>
+              </Col>
+              <Col span={24}>
+                <Form.Item
+                  name="fullName"
+                  label="Nombre completo"
+                  rules={[
+                    {
+                      required: true,
+                    },
+                    {
+                      max: 255,
+                    },
+                  ]}
+                >
+                  <Input disabled={hasInitialValues} />
+                </Form.Item>
+              </Col>
+              <Col xs={24} md={12}>
+                <Form.Item
+                  name="identification"
+                  label="Número de identidad"
+                  tooltip="Escriba el número de identidad sin puntos ni espacios"
+                  rules={[
+                    {
+                      pattern: /^[0-9]+$/,
+                      message: "El número de identidad solo puede contener números",
+                    },
+                    {
+                      required: true,
+                    },
+                    {
+                      max: 100,
+                    },
+                  ]}
+                >
+                  <Input disabled={hasInitialValues} />
+                </Form.Item>
+              </Col>
+              <Col xs={24} md={12}>
+                <Form.Item
+                  name="email"
+                  label="E-mail"
+                  rules={[
+                    {
+                      type: "email",
+                    },
+                    {
+                      required: true,
+                    },
+                    {
+                      max: 150,
+                    },
+                  ]}
+                >
+                  <Input disabled={hasInitialValues} />
+                </Form.Item>
+              </Col>
+              <Col span={24}>
+                <Form.Item
+                  name="cellphone"
+                  label="Teléfono celular"
+                  tooltip="Escriba el número de celular sin puntos ni espacios"
+                  rules={[
+                    { required: true },
+                    {
+                      pattern: /^[0-9]+$/,
+                      message: "El teléfono solo puede contener números",
+                    },
+                    {
+                      max: 20,
+                    },
+                  ]}
+                >
+                  <Input
+                    addonBefore={
+                      <Form.Item name="prefix" rules={[{ required: true }]} noStyle>
+                        <CustomSelectWithInput
+                          selectProps={{
+                            options: [
+                              {
+                                value: "57",
+                                label: "+57",
+                              },
+                            ],
+                            style: { width: 75 },
+                            popupMatchSelectWidth: false,
+                          }}
+                          inputProps={{
+                            placeholder: "Escriba...",
+                            style: {
+                              width: 65,
+                            },
+                          }}
+                        />
+                      </Form.Item>
+                    }
+                    style={{ width: "100%" }}
+                  />
+                </Form.Item>
+              </Col>
+              <Col span={24}>
+                <Form.Item
+                  name="address"
+                  label="Dirección"
+                  rules={[
+                    {
+                      required: true,
+                    },
+                    {
+                      max: 255,
+                    },
+                  ]}
+                >
+                  <Input />
+                </Form.Item>
+              </Col>
+              <Col xs={24} md={12}>
+                <Form.Item
+                  name="age"
+                  label="Edad"
+                  rules={[
+                    {
+                      type: "number",
+                      min: 0,
+                      max: 120,
+                    },
+                    {
+                      type: "number",
+                    },
+                  ]}
+                >
+                  <InputNumber min={0} style={{ width: '100%' }} />
+                </Form.Item>
+              </Col>
+              <Col xs={24} md={12}>
+                <Form.Item
+                  name="gender"
+                  label="Género"
+                  rules={[{ type: "enum", enum: Object.values(VendorGenders) }]}
+                >
+                  <Radio.Group
+                    options={[
+                      { value: "M", label: "Hombre" },
+                      { value: "F", label: "Mujer" },
+                      { value: "O", label: "Otro" },
+                    ]}
+                  />
+                </Form.Item>
+              </Col>
+              <Col span={24}>
+                <Form.Item name={"bio"} label="Biografía" rules={[]}>
+                  <Input.TextArea rows={3} />
+                </Form.Item>
+              </Col>
+            </Row>
+          </Col>
+
+          {/* Columna derecha - Información financiera */}
+          <Col xs={24} lg={12}>
             <Divider>Información financiera</Divider>
-            <Form.Item
-              name={"commercialRegistry"}
-              label="Registro comercial"
-              rules={[
-                {
-                  max: 255,
-                },
-              ]}
-            >
-              <Input disabled={hasInitialValues} />
-            </Form.Item>
-            <Form.Item
-              name={"rut"}
-              label="RUT"
-              rules={[
-                {
-                  max: 255,
-                },
-              ]}
-            >
-              <Input disabled={hasInitialValues} />
-            </Form.Item>
-            <Form.Item
-              name={["bankAccount", "entity"]}
-              label="Banco"
-              rules={[
-                ({ getFieldValue }) => ({
-                  validator(_, value) {
-                    const number = getFieldValue(["bankAccount", "number"]);
-                    const type = getFieldValue(["bankAccount", "type"]);
-                    if ((number || type) && !value) {
-                      return Promise.reject(
-                        "Campo requerido si otros campos bancarios están llenos"
-                      );
-                    }
-                    return Promise.resolve();
-                  },
-                }),
-              ]}
-            >
-              <Input />
-            </Form.Item>
-            <Form.Item
-              name={["bankAccount", "number"]}
-              label="Número de cuenta"
-              rules={[
-                ({ getFieldValue }) => ({
-                  validator(_, value) {
-                    const entity = getFieldValue(["bankAccount", "entity"]);
-                    const type = getFieldValue(["bankAccount", "type"]);
-                    if ((entity || type) && !value) {
-                      return Promise.reject(
-                        "Campo requerido si otros campos bancarios están llenos"
-                      );
-                    }
-                    return Promise.resolve();
-                  },
-                }),
-              ]}
-            >
-              <Input />
-            </Form.Item>
-            <Form.Item
-              name={["bankAccount", "type"]}
-              label="Tipo de cuenta"
-              rules={[
-                ({ getFieldValue }) => ({
-                  validator(_, value) {
-                    const entity = getFieldValue(["bankAccount", "entity"]);
-                    const number = getFieldValue(["bankAccount", "number"]);
-                    if ((entity || number) && !value) {
-                      return Promise.reject(
-                        "Campo requerido si otros campos bancarios están llenos"
-                      );
-                    }
-                    return Promise.resolve();
-                  },
-                }),
-              ]}
-            >
-              <CustomSelectWithInput
-                selectProps={{
-                  options: [
+            <Row gutter={[16, 16]}>
+              <Col span={24}>
+                <Form.Item
+                  name={"commercialRegistry"}
+                  label="Registro comercial"
+                  rules={[
                     {
-                      value: "Ahorros",
+                      max: 255,
                     },
+                  ]}
+                >
+                  <Input disabled={hasInitialValues} />
+                </Form.Item>
+              </Col>
+              <Col span={24}>
+                <Form.Item
+                  name={"rut"}
+                  label="RUT"
+                  rules={[
                     {
-                      value: "Corriente",
+                      max: 255,
                     },
-                  ],
-                  allowClear: true,
-                }}
-                inputProps={{
-                  placeholder: "Escriba...",
-                }}
-              />
-            </Form.Item>
-          </div>
-        </div>
+                  ]}
+                >
+                  <Input disabled={hasInitialValues} />
+                </Form.Item>
+              </Col>
+              <Col span={24}>
+                <Form.Item
+                  name={["bankAccount", "entity"]}
+                  label="Banco"
+                  rules={[
+                    ({ getFieldValue }) => ({
+                      validator(_, value) {
+                        const number = getFieldValue(["bankAccount", "number"]);
+                        const type = getFieldValue(["bankAccount", "type"]);
+                        if ((number || type) && !value) {
+                          return Promise.reject(
+                            "Campo requerido si otros campos bancarios están llenos"
+                          );
+                        }
+                        return Promise.resolve();
+                      },
+                    }),
+                  ]}
+                >
+                  <Input />
+                </Form.Item>
+              </Col>
+              <Col xs={24} md={12}>
+                <Form.Item
+                  name={["bankAccount", "number"]}
+                  label="Número de cuenta"
+                  rules={[
+                    ({ getFieldValue }) => ({
+                      validator(_, value) {
+                        const entity = getFieldValue(["bankAccount", "entity"]);
+                        const type = getFieldValue(["bankAccount", "type"]);
+                        if ((entity || type) && !value) {
+                          return Promise.reject(
+                            "Campo requerido si otros campos bancarios están llenos"
+                          );
+                        }
+                        return Promise.resolve();
+                      },
+                    }),
+                  ]}
+                >
+                  <Input />
+                </Form.Item>
+              </Col>
+              <Col xs={24} md={12}>
+                <Form.Item
+                  name={["bankAccount", "type"]}
+                  label="Tipo de cuenta"
+                  rules={[
+                    ({ getFieldValue }) => ({
+                      validator(_, value) {
+                        const entity = getFieldValue(["bankAccount", "entity"]);
+                        const number = getFieldValue(["bankAccount", "number"]);
+                        if ((entity || number) && !value) {
+                          return Promise.reject(
+                            "Campo requerido si otros campos bancarios están llenos"
+                          );
+                        }
+                        return Promise.resolve();
+                      },
+                    }),
+                  ]}
+                >
+                  <CustomSelectWithInput
+                    selectProps={{
+                      options: [
+                        {
+                          value: "Ahorros",
+                        },
+                        {
+                          value: "Corriente",
+                        },
+                      ],
+                      allowClear: true,
+                    }}
+                    inputProps={{
+                      placeholder: "Escriba...",
+                    }}
+                  />
+                </Form.Item>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
       )}
     </FormWrapper>
   );
